a2eb8525d3420f0d84dec9022f8b3e61192c5784
Dimas Erick Rivas Torres <dimaserickrivastorres@Dimass-iMac.local>
2018-05-20 22:09:30 -0400

    adding options using the click package, and add the start and stop option based on the project

--- code/code.py
+++ code/code.py
@@ -1,11 +1,60 @@
 import boto3
+import click
 
-session = boto3.session(profile_name='3r1ck')
+session = boto3.Session(profile_name='3r1ck')
 ec2 = session.resource('ec2')
 
-def list_instances():
-    for i in ec2.instances.all():
-        print(i)
+def filter_instances(project):
+    instances = []
+    if project:
+        filters = [{'Name':'tag:Project','Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Command for instances"""
+@instances.command('list')
+@click.option('--project',default=None, help="only instances for project(tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+    instances = filter_instances(project)
+
+    for i in instances:
+
+        print(', '.join((
+            i.id,
+            i.instance_type,
+            i.placement['AvailabilityZone'],
+            i.state['Name'],
+            i.public_dns_name
+            )))
+    return
+
+@instances.command('stop')
+@click.option('--project',default=None, help="only instances for project(tag Project:<name>)")
+def stop_instances(project):
+    "Stops EC2 Instances by project (TAG)"
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+    return
+
+@instances.command('start')
+@click.option('--project',default=None, help="only instances for project(tag Project:<name>)")
+def start_instances(project):
+    "Start EC2 Instances by project (TAG)"
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+    return
 
 if __name__ == '__main__':
-    list_instances()
+    instances()
